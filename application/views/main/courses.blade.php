cf3cb28823ef07dc62ee114b038adfed34a2f1d8
Nathan Yam <nathancyam@gmail.com>
2013-05-12 22:06:00 +1000

    fixed rules for courses

--- application/views/main/courses.blade.php
+++ application/views/main/courses.blade.php
@@ -6,15 +6,19 @@
         <div style="float: left;">
             <h1>Courses</h1>
         </div>
-        <div style='float: right;'>
-            {{ HTML::link_to_route('newcourse','Add a new course') }}
-        </div>
+        @if (Auth::user()->role == 'admin')
+            <div style='float: right;'>
+                {{ HTML::link_to_route('newcourse','Add a new course') }}
+            </div>
+        @endif
     </div>
     @foreach($courses as $acourse)
         <div style="container-fluid">
             <h3>{{ $acourse->code }}: {{ $acourse->name }}</h3>
             <p>{{ HTML::link(URL::to_route('listsubjects', array($acourse->code)), 'See subjects for ' . $acourse->name) }}</p>
-            <p>{{ HTML::link(URL::to_route('editcourse', array($acourse->code)), 'Update this course: ' . $acourse->name) }}</p>
+            @if (Auth::user()->role == 'admin')
+                <p>{{ HTML::link(URL::to_route('editcourse', array($acourse->code)), 'Update this course: ' . $acourse->name) }}</p>
+            @endif
         </div>
     @endforeach
 </div>
