0aae85a437251d33b93d8e83f0f169d513efe4b8
Nathan Yam <nathancyam@gmail.com>
2013-05-12 22:09:47 +1000

    Fixed topic rules

--- application/views/main/topics.blade.php
+++ application/views/main/topics.blade.php
@@ -7,9 +7,11 @@
         <div style="float: left;">
             <h2>Topics for {{ $course->name }}: {{ $subject->name }}</h2>
         </div>
+        @if (Auth::user()->role == 'admin')
         <div style="float: right;">
             {{ Button::primary_link(URL::to_route('newtopic', array($course->code, $subject->code)), 'New Topic') }}
         </div>
+        @endif
     </div>
     @foreach($topics as $topic)
         <div class="container-fluid">
@@ -17,8 +19,12 @@
                 {{ $topic->name }}
             </h3>
             <p>{{ $topic->content }}</p>
-            <p>{{ Button::small_link(URL::to_route('listposts', array($course->code, $subject->code, $topic->id)), 'See posts') }}
-            {{ Button::small_link(URL::to_route('edittopic', array($course->code, $subject->code, $topic->id)), 'Edit topic') }}</p>
+            <p>
+                {{ Button::small_link(URL::to_route('listposts', array($course->code, $subject->code, $topic->id)), 'See posts') }}
+                @if (Auth::user()->role == 'admin')
+                    {{ Button::small_link(URL::to_route('edittopic', array($course->code, $subject->code, $topic->id)), 'Edit topic') }}
+                @endif
+            </p>
             <hr />
         </div>
     @endforeach
