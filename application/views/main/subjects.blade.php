6f6aace3b7d551ff1f3840c63391c270bcfb99c0
Nathan Yam <nathancyam@gmail.com>
2013-05-12 22:08:08 +1000

    Fixed subject rules

--- application/views/main/subjects.blade.php
+++ application/views/main/subjects.blade.php
@@ -6,16 +6,20 @@
             <div style="float: left;">
                 <h2>Subjects for {{ $code->name }}</h2>
             </div>
-            <div style="float: right;">
-                {{ Button::primary_link(URL::base() . '/' . $code->code . '/subject/new', 'Create a new subject')}}
-            </div>
+            @if(Auth::user()->role == 'admin')
+                <div style="float: right;">
+                    {{ Button::primary_link(URL::base() . '/' . $code->code . '/subject/new', 'Create a new subject')}}
+                </div>
+            @endif
         </div>
         @foreach($subjects as $subject)
             <div class="container-fluid">
                 <h3>{{ $subject->code }}: {{ $subject->name }}</h3>
                 <p>{{ $subject->description }}</p>
                 <p>{{ Button::small_link(URL::to_route('listtopics', array($code->code, $subject->code)), 'List topics') }}
-                {{ Button::small_link(URL::to_route('editsubject', array($code->code, $subject->code)), 'Edit Subject') }}</p>
+                @if(Auth::user()->role == 'admin')
+                    {{ Button::small_link(URL::to_route('editsubject', array($code->code, $subject->code)), 'Edit Subject') }}</p>
+                @endif
                 <hr />
             </div>
         @endforeach
