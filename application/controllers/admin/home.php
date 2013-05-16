84eb4ed9f3efd9a7b02053d5eeaa4b13ee44b35c
Nathan Yam <nathancyam@gmail.com>
2013-05-16 20:18:13 +1000

    Some tab fixes

--- application/controllers/admin/home.php
+++ application/controllers/admin/home.php
@@ -2,16 +2,16 @@
 
 class Admin_Home_Controller extends Base_Controller
 {
-	public function __construct()
-	{
-		parent::__construct();
-		$this->filter('before','auth');
-		$this->filter('before','admin');
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->filter('before','auth');
+        $this->filter('before','admin');
+    }
 
-	public function action_index()
-	{
-		return View::make('admin.panel');
-	}
+    public function action_index()
+    {
+        return View::make('admin.panel');
+    }
 
 }
--- application/controllers/admin/users.php
+++ application/controllers/admin/users.php
@@ -2,87 +2,87 @@
 
 class Admin_Users_Controller extends Base_Controller
 {
-	public function __construct(){
-		parent::__construct();
-		$this->filter('before','auth');
-		$this->filter('before','onlyadmin');
-	}
+    public function __construct(){
+        parent::__construct();
+        $this->filter('before','auth');
+        $this->filter('before','onlyadmin');
+    }
 
-	public function action_index()
-	{
-		$table = array();
-		$users = User::all();
-		foreach($users as $user){
-			$userrole = $user->role;
-			switch($userrole){
-				case "admin":
-					$userlabel = Label::important("Admin");
-					break;
-				case "coordinator":
-					$userlabel = Label::success("Coordinator");
-					break;
-				case "teacher":
-					$userlabel = Label::info("Teacher");
-					break;
-				case "student":
-					$userlabel = Label::inverse("Student");
-					break;
-				default:
-					$userlabel = "";
-					break;
-			}
-			$editUser = HTML::link_to_action('admin.users@edit','Edit',array($user->id));
-			$row = array(
-						'fname' => $user->fname,
-						'sname' => $user->sname,
-						'email' => $user->email,
-						'role' => $userlabel,
-						'link' => $editUser);
-			array_push($table, $row);
-		}
-		return View::make('admin.users')
-			->with('table', $table);
-	}
+    public function action_index()
+    {
+        $table = array();
+        $users = User::all();
+        foreach($users as $user){
+            $userrole = $user->role;
+            switch($userrole){
+            case "admin":
+                $userlabel = Label::important("Admin");
+                break;
+            case "coordinator":
+                $userlabel = Label::success("Coordinator");
+                break;
+            case "teacher":
+                $userlabel = Label::info("Teacher");
+                break;
+            case "student":
+                $userlabel = Label::inverse("Student");
+                break;
+            default:
+                $userlabel = "";
+                break;
+            }
+            $editUser = HTML::link_to_action('admin.users@edit','Edit',array($user->id));
+            $row = array(
+                'fname' => $user->fname,
+                'sname' => $user->sname,
+                'email' => $user->email,
+                'role' => $userlabel,
+                'link' => $editUser);
+            array_push($table, $row);
+        }
+        return View::make('admin.users')
+            ->with('table', $table);
+    }
 
-	// Here we use AJAX to populate the tables. See the appropriate Javascript
-	public function action_role($role)
-	{
-		$editUser = HTML::link_to_action('course');
-		$table = array();
-		$users = User::where('role','=',$role)->get();
-		foreach($users as $user){
-			switch($role){
-				case "admin":
-					$userlabel = Label::important("Admin");
-					break;
-				case "coordinator":
-					$userlabel = Label::success("Coordinator");
-					break;
-				case "teacher":
-					$userlabel = Label::info("Teacher");
-					break;
-				case "student":
-					$userlabel = Label::inverse("Student");
-					break;
-				default:
-					$userlabel = "";
-					break;
-			}
-			$editUser = HTML::link_to_action('admin.users@edit','Edit',array($user->id));
-			$row = array(
-						'fname' => $user->fname,
-						'sname' => $user->sname,
-						'email' => $user->email,
-						'role' => $userlabel,
-						'link' => $editUser);
-			array_push($table, $row);
-		}
-		return View::make('template.usertable')
-			->with('table', $table);
-	}
+    // Here we use AJAX to populate the tables. See the appropriate Javascript
+    public function action_role($role)
+    {
+        $editUser = HTML::link_to_action('course');
+        $table = array();
+        $users = User::where('role','=',$role)->get();
+        foreach($users as $user){
+            switch($role){
+            case "admin":
+                $userlabel = Label::important("Admin");
+                break;
+            case "coordinator":
+                $userlabel = Label::success("Coordinator");
+                break;
+            case "teacher":
+                $userlabel = Label::info("Teacher");
+                break;
+            case "student":
+                $userlabel = Label::inverse("Student");
+                break;
+            default:
+                $userlabel = "";
+                break;
+            }
+            $editUser = HTML::link_to_action('admin.users@edit','Edit',array($user->id));
+            $row = array(
+                'fname' => $user->fname,
+                'sname' => $user->sname,
+                'email' => $user->email,
+                'role' => $userlabel,
+                'link' => $editUser);
+            array_push($table, $row);
+        }
+        return View::make('template.usertable')
+            ->with('table', $table);
+    }
 
-	public function action_edit($user_id)
-	{
-		$editUser = User::find($user_id);
-	}
+    public function action_edit($user_id)
+    {
+        $editUser = User::find($user_id);
+    }
 }
