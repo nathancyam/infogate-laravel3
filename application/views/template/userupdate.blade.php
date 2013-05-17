61d45f99471aaac62ece879c848202e96fb87ad1
Nathan Yam <nathancyam@gmail.com>
2013-05-17 14:53:43 +1000

    Removed user update

--- application/views/template/userupdate.blade.php
+++
@@ -1,4 +0,0 @@
-{{ Form::open(URL::to_action('admin.users@update')) }}
-{{ Form::inline_open() }}
-{{ Form::text('fName', null, array('class'=>'input-small', 'placeholder'=>$user->fname)) }}
-{{ Form::text('sName', null, array('class'=>'input-small', 'placeholder'=>$user->sname)) }}
