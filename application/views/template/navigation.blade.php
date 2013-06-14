cdc642d918c16e0736e3449d7bb9983e053d464e
Nathan Yam <nathancyam@gmail.com>
2013-06-14 21:43:54 +1000

    Added icons to nav menu

--- application/views/template/navigation.blade.php
+++ application/views/template/navigation.blade.php
@@ -2,25 +2,24 @@
 
 $navArray = array(
         array(Navigation::HEADER, 'Navigation'),
-        array('Home', URL::base()),
-        array('User Panel', URL::to_action('account')));
+        array('User Panel', URL::to_action('account'), false, false, null, 'off'));
 
 if (Auth::user()->role == 'admin'){
     array_push($navArray, array(Navigation::HEADER, 'Admin Panel'));
-    array_push($navArray, array('Admin Home', URL::to_action('admin')));
-    array_push($navArray, array('Courses', URL::to_route('listcourses')));
-    array_push($navArray, array('Users', URL::to_action('admin@users')));
+    array_push($navArray, array('Admin Home', URL::to_action('admin'), false, false, null, 'lock' ));
+    array_push($navArray, array('Courses', URL::to_route('listcourses'), false, false, null, 'list-alt'));
+    array_push($navArray, array('Users', URL::to_action('admin@users'), false, false, null, 'user' ));
 }
 
 array_push($navArray, array(Navigation::HEADER, 'Courses'));
-array_push($navArray, array(strtoupper($code), URL::to_route('listsubjects', array($code))));
+array_push($navArray, array(strtoupper($code), URL::to_route('listsubjects', array($code)), false, false, null, 'briefcase'));
 
 array_push($navArray, array(Navigation::HEADER, 'Subjects for ' . strtoupper($code)));
 foreach($subjects as $subject){
-    array_push($navArray, array(strtoupper($subject->code), URL::to_route('listtopics', array($code, $subject->code))));
+    array_push($navArray, array(strtoupper($subject->code), URL::to_route('listtopics', array($code, $subject->code)), false, false, null, 'pencil'));
 }
 
 array_push($navArray, array(Navigation::HEADER, 'Help'));
-array_push($navArray, array('Help', URL::to_route('help')));
+array_push($navArray, array('Help', URL::to_route('help'), false, false, null, 'question-sign'));
 
 echo Navigation::lists(Navigation::links($navArray), true, array('class'=>'sidenav affix'));
