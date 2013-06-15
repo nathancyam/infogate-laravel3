41507e0d1e6067a372bfbae17af6e9db87d6e0b8
Nathan Yam <nathancyam@gmail.com>
2013-06-15 15:09:10 +1000

    Carasoul for remote

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -6,13 +6,16 @@
 
 <?php
     $imgGallery = array();
-    array_push($imgGallery, array('image'=>'img/carousel-image-library',
+    $image01 = 'http://laravelinfogate-env-nxfwkz5cpg.elasticbeanstalk.com/img/carousel-image-library.jpg';
+    $image02 = 'http://laravelinfogate-env-nxfwkz5cpg.elasticbeanstalk.com/img/carousel-image-sharing.jpg';
+    $image03 = 'http://laravelinfogate-env-nxfwkz5cpg.elasticbeanstalk.com/img/carousel-image-table.jpg';
+    array_push($imgGallery, array('image'=>$image01,
                     'label'=>'Learning Materials Everywhere',
                     'caption'=>'A place to find relevant eductional information about your work'));
-    array_push($imgGallery, array('image'=>'img/carousel-image-sharing',
+    array_push($imgGallery, array('image'=>$image02,
                     'label'=>'Get approved information regarding your work',
                     'caption'=>'Learning materials found by your collegues, shared for everyone.'));
-    array_push($imgGallery, array('image'=>'img/carousel-image-table',
+    array_push($imgGallery, array('image'=>$image03,
                     'label'=>'Collaborate and discuss',
                     'caption'=>'A place for students to talk about sources and information'));
     echo Carousel::create($imgGallery, array('style'=>'width: 900px;'));
